9c05a3846073eff0f7aef0b902930cfd8adcac17
rahulshah9713 <56013409+rahulshah9713@users.noreply.github.com>
2020-05-27 10:32:56 +0530

    Added dropdown example

---
+++ dropdown_example.py
@@ -0,0 +1,17 @@
+import kivy 
+from kivy.app import App 
+from kivy.uix.dropdown import DropDown 
+from kivy.uix.button import Button 
+from kivy.base import runTouchApp 
+
+dropdown = DropDown() 
+for index in range(10): 
+	btn = Button(text ='Value % d' % index, size_hint_y = None, height = 40) 
+	btn.bind(on_release = lambda btn: dropdown.select(btn.text)) 
+	dropdown.add_widget(btn) 
+
+mainbutton = Button(text ='Hello', size_hint =(None, None), pos =(350, 400)) 
+mainbutton.bind(on_release = dropdown.open) 
+
+dropdown.bind(on_select = lambda instance, x: setattr(mainbutton, 'text', x)) 
+runTouchApp(mainbutton)
